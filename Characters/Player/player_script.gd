d2bd5683dcc59f66e48631f4e82044095a4ec5ab
Dhaniel Bolosan <dhaniel808123@gmail.com>
2026-03-05 23:16:08 -1000

    Basic AnimationTree + Soul Switch

--- Characters/Player/player_script.gd
+++ Characters/Player/player_script.gd
@@ -4,16 +4,42 @@ extends CharacterBody3D
 @onready var anim_tree = $Model/AnimationTree
 @onready var model = $Model
 
-@export var blend_speed = 15
+@export var blend_speed = 30
 
 enum {IDLE, RUN}
-var run_val = 0.0
+enum {DEMON, HUNTER, SWORD}
+var soul_blend_paths = {
+	DEMON: ["Demon", "parameters/Demon_Blend/blend_amount"],
+	HUNTER: ["Hunter", "parameters/Hunter_Blend/blend_amount"],
+	SWORD: ["Sword", "parameters/Sword_Blend/blend_amount"]
+}
+
 var currAnim = IDLE
+var currSoul = HUNTER
+var is_switching = false
 
-const SPEED = 5.0
+const SPEED = 10.0
+var run_val = 0.0
 
+func check_soul_switch():
+	if Input.is_action_just_pressed("ui_soul_switch") and !is_switching:
+		is_switching = true
+		Engine.time_scale = 0.2		
+		
+		currSoul = (int(currSoul) + 1) % 3
+		var soul_anim = soul_blend_paths[currSoul]
+		
+		anim_tree["parameters/Soul_Switch_Trans/transition_request"] = soul_anim[0]
+		anim_tree["parameters/Soul_Switch/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE
+		
+		await get_tree().create_timer(1.0, true, false, true).timeout
+		
+		Engine.time_scale = 1
+		is_switching = false
+		
 func update_anim_tree():
-	anim_tree["parameters/Run/blend_amount"] = run_val;
+	var current_blend_path = soul_blend_paths[currSoul][1]
+	anim_tree[current_blend_path] = run_val
 	
 func handle_animations(delta):
 	match currAnim:
@@ -23,8 +49,12 @@ func handle_animations(delta):
 			run_val = lerpf(run_val, 1, blend_speed * delta)
 
 func _physics_process(delta):
-	handle_animations(delta)
 	update_anim_tree()
+	handle_animations(delta)
+	
+	if is_switching:
+		move_and_slide()
+		return
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -45,5 +75,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		currAnim = IDLE
-
+		
+	check_soul_switch()
 	move_and_slide()
